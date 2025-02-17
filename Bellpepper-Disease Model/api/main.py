1820cd3e0ae91cfb383885eac7125680776f5538
Disandu Sanhida Jayawardhane <73415096+Ds-Jayawardhana@users.noreply.github.com>
2025-02-17 05:40:39 +0530

    Update main.py

--- Bellpepper-Disease Model/api/main.py
+++ Bellpepper-Disease Model/api/main.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 app = FastAPI()
 
 
-MODEL = tf.keras.models.load_model("api/bellpepper_model.h5")
+MODEL = tf.keras.models.load_model("bellpepper_model.h5")
 
 CLASS_NAMES = ["Pepper,_bell___Bacterial_spot", "Pepper,_bell___healthy"]
 
